abc592758d531168898fa2cd99fdbad77bf26772
Viraj Bangari <Viraj3f@gmail.com>
2016-12-03 10:55:50 -0500

    Add working db

--- server.py
+++ server.py
@@ -13,14 +13,14 @@ database.init()
 @socketio.on('update')
 def update(drawing):
     pprint(drawing)
+    database.insert_drawing(drawing)
     socketio.emit("recieve", drawing)  # Send the updated sketch to client
-    data.append(drawing)
 
 
 @socketio.on('join')
 def connect_user(message):
     print(message + "...success!")
-    emit("loadDrawing", data)
+    emit("loadDrawing", database.get_whole_drawing())
 
 
 @app.route('/<session_id>/draw')
@@ -33,5 +33,4 @@ def hello_world():
     return 'It works!'
 
 if __name__ == '__main__':
-    #  socketio.run(app, host="0.0.0.0", port=8000)
-    conn.close()
+    socketio.run(app, host="0.0.0.0", port=8000)
